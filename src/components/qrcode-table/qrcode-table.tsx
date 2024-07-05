bd8ed424788b1e13bb243a1ddeff47b829b8fa41
Dien Cap Bao <dien.capbao@sharpensols.com>
2024-07-05 11:53:40 +0700

    feat: Add more info into QR code

--- src/components/qrcode-table/qrcode-table.tsx
+++ src/components/qrcode-table/qrcode-table.tsx
@@ -8,25 +8,44 @@ import { getTableLink } from '@/lib/utils'
 interface QRCodeTableProps {
   tableNumber: number
   token: string
+  width?: number
 }
 
-export default function QRCodeTable({ tableNumber, token }: QRCodeTableProps) {
+export default function QRCodeTable({ tableNumber, token, width = 250 }: QRCodeTableProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const canvas = canvasRef.current
+    const canvas = canvasRef.current!
+    canvas.height = width + 70
+    canvas.width = width
+    const canvasContext = canvas.getContext('2d')!
+    canvasContext.fillStyle = '#fff'
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    canvasContext.font = '20px Arial'
+    canvasContext.textAlign = 'center'
+    canvasContext.fillStyle = '#000'
+    canvasContext.fillText(`Bàn số ${tableNumber}`, canvas.width / 2, canvas.width + 20)
+    canvasContext.fillText('Quét mã QR để gọi món', canvas.width / 2, canvas.width + 50)
+
+    const virtualCanvas = document.createElement('canvas')
 
     QRCode.toCanvas(
-      canvas,
+      virtualCanvas,
       getTableLink({
         tableNumber,
         token
       }),
+      {
+        width,
+        margin: 4
+      },
       function (error) {
         if (error) console.error(error)
+
+        canvasContext.drawImage(virtualCanvas, 0, 0, width, width)
       }
     )
-  }, [tableNumber, token])
+  }, [tableNumber, token, width])
 
   return <canvas ref={canvasRef} />
 }
