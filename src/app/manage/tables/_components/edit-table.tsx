32ad02138f6feaa71bbc680d8df5a20001ea3aa8
Dien Cap Bao <dien.capbao@sharpensols.com>
2024-07-09 11:01:11 +0700

    feat: Handle edit a table

--- src/app/manage/tables/_components/edit-table.tsx
+++ src/app/manage/tables/_components/edit-table.tsx
@@ -3,18 +3,29 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import Link from 'next/link'
+import { useEffect } from 'react'
 
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription
+} from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
-import { getTableLink, getVietnameseTableStatus } from '@/lib/utils'
+import { getTableLink, getVietnameseTableStatus, handleErrorApi } from '@/lib/utils'
 import { UpdateTableBody, UpdateTableBodyType } from '@/schemaValidations/table.schema'
 import { TableStatus, TableStatusValues } from '@/constants/type'
+import { useTableDetailQuery, useUpdateTableMutation } from '@/hooks'
+import { useToast } from '@/components/ui/use-toast'
+import QRCodeTable from '@/components/qrcode-table'
 
 export function EditTable({
   id,
@@ -25,6 +36,10 @@ export function EditTable({
   setId: (value: number | undefined) => void
   onSubmitSuccess?: () => void
 }) {
+  const toast = useToast()
+  const { data } = useTableDetailQuery(id as number)
+  const updateTableMutation = useUpdateTableMutation()
+
   const form = useForm<UpdateTableBodyType>({
     resolver: zodResolver(UpdateTableBody),
     defaultValues: {
@@ -33,7 +48,36 @@ export function EditTable({
       changeToken: false
     }
   })
-  const tableNumber = 0
+
+  useEffect(() => {
+    if (data) {
+      const { capacity, status } = data.payload.data
+      form.reset({
+        capacity,
+        status,
+        changeToken: form.getValues('changeToken')
+      })
+    }
+  }, [data, form])
+
+  const handleUpdateTable = async (formValues: UpdateTableBodyType) => {
+    if (updateTableMutation.isPending) return
+
+    try {
+      let payload: UpdateTableBodyType & { id: number } = { ...formValues, id: id as number }
+
+      const response = await updateTableMutation.mutateAsync(payload)
+      toast.toast({ description: response.payload.message })
+      onSubmitSuccess?.()
+      reset()
+    } catch (error) {
+      handleErrorApi({ error, setError: form.setError })
+    }
+  }
+
+  const reset = () => {
+    setId(undefined)
+  }
 
   return (
     <Dialog
@@ -53,15 +97,28 @@ export function EditTable({
       >
         <DialogHeader>
           <DialogTitle>Cập nhật bàn ăn</DialogTitle>
+          <DialogDescription></DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form noValidate className="grid auto-rows-max items-start gap-4 md:gap-8" id="edit-table-form">
+          <form
+            noValidate
+            className="grid auto-rows-max items-start gap-4 md:gap-8"
+            id="edit-table-form"
+            onSubmit={form.handleSubmit(handleUpdateTable, console.log)}
+            onReset={reset}
+          >
             <div className="grid gap-4 py-4">
               <FormItem>
                 <div className="grid grid-cols-4 items-center justify-items-start gap-4">
                   <Label htmlFor="name">Số hiệu bàn</Label>
                   <div className="col-span-3 w-full space-y-2">
-                    <Input id="number" type="number" className="w-full" value={tableNumber} readOnly />
+                    <Input
+                      id="number"
+                      type="number"
+                      className="w-full"
+                      value={data?.payload.data.number || 0}
+                      readOnly
+                    />
                     <FormMessage />
                   </div>
                 </div>
@@ -131,26 +188,30 @@ export function EditTable({
               <FormItem>
                 <div className="grid grid-cols-4 items-center justify-items-start gap-4">
                   <Label>QR Code</Label>
-                  <div className="col-span-3 w-full space-y-2"></div>
+                  <div className="col-span-3 w-full space-y-2">
+                    {data && <QRCodeTable tableNumber={data.payload.data.number} token={data.payload.data.token} />}
+                  </div>
                 </div>
               </FormItem>
               <FormItem>
                 <div className="grid grid-cols-4 items-center justify-items-start gap-4">
                   <Label>URL gọi món</Label>
                   <div className="col-span-3 w-full space-y-2">
-                    <Link
-                      href={getTableLink({
-                        token: '123123123',
-                        tableNumber: tableNumber
-                      })}
-                      target="_blank"
-                      className="break-all"
-                    >
-                      {getTableLink({
-                        token: '123123123',
-                        tableNumber: tableNumber
-                      })}
-                    </Link>
+                    {data && (
+                      <Link
+                        href={getTableLink({
+                          token: data.payload.data.token,
+                          tableNumber: data.payload.data.number
+                        })}
+                        target="_blank"
+                        className="break-all"
+                      >
+                        {getTableLink({
+                          token: data.payload.data.token,
+                          tableNumber: data.payload.data.number
+                        })}
+                      </Link>
+                    )}
                   </div>
                 </div>
               </FormItem>
