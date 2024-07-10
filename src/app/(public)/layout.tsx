94c477288eed543574c0a2b6f516a99527e56905
Dien Cap Bao <dien.capbao@sharpensols.com>
2024-07-10 11:52:58 +0700

    feat: Fetch dish list in home page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({
 }>) {
   return (
     <div className="flex min-h-screen w-full flex-col relative">
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <header className="sticky z-20 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
             <Package2 className="h-6 w-6" />
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,86 +1,58 @@
+import { formatCurrency } from '@/lib/utils'
+import { DishListResType } from '@/schemaValidations/dish.schema'
+import { dishService } from '@/services'
 import Image from 'next/image'
 
-export default function HomePage() {
+export default async function Home() {
+  let dishList: DishListResType['data'] = []
+
+  try {
+    const response = await dishService.getAll()
+    dishList = response.payload?.data || []
+  } catch (error) {
+    dishList = []
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <div className="w-full space-y-4">
+      <div className="relative z-10">
+        <span className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></span>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/banner.png"
+          width={400}
+          height={200}
+          quality={100}
+          alt="Banner"
+          className="absolute top-0 left-0 w-full h-full object-cover"
         />
+        <div className="z-20 relative py-10 md:py-20 px-4 sm:px-10 md:px-20">
+          <h1 className="text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold">Nhà hàng Big Boy</h1>
+          <p className="text-center text-sm sm:text-base mt-4">Vị ngon, trọn khoảnh khắc</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <section className="space-y-10 py-16">
+        <h2 className="text-center text-2xl font-bold">Đa dạng các món ăn</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {dishList.map((dish) => (
+            <div className="flex gap-4 w" key={dish.id}>
+              <div className="flex-shrink-0">
+                <Image
+                  src={dish.image}
+                  width={150}
+                  height={150}
+                  alt={dish.name}
+                  className="object-cover w-[150px] h-[150px] rounded-md"
+                />
+              </div>
+              <div className="space-y-1">
+                <h3 className="text-xl font-semibold">{dish.name}</h3>
+                <p className="">{dish.description}</p>
+                <p className="font-semibold">{formatCurrency(dish.price)}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   )
 }
