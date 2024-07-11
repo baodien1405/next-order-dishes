b095aec66c0cebf2257dde977fe0cf90bb44a570
Dien Cap Bao <dien.capbao@sharpensols.com>
2024-07-11 14:24:58 +0700

    feat: Add UI guest login

---
+++ src/app/(public)/tables/[number]/_components/guest-login-form.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { GuestLoginBody, GuestLoginBodyType } from '@/schemaValidations/guest.schema'
+
+export function GuestLoginForm() {
+  const form = useForm<GuestLoginBodyType>({
+    resolver: zodResolver(GuestLoginBody),
+    defaultValues: {
+      name: '',
+      token: '',
+      tableNumber: 1
+    }
+  })
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Đăng nhập gọi món</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form className="space-y-2 max-w-[600px] flex-shrink-0 w-full" noValidate>
+            <div className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="grid gap-2">
+                      <Label htmlFor="name">Tên khách hàng</Label>
+                      <Input id="name" type="text" required {...field} />
+                      <FormMessage />
+                    </div>
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full">
+                Đăng nhập
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/app/(public)/tables/[number]/page.tsx
@@ -0,0 +1,5 @@
+import { GuestLoginForm } from '@/app/(public)/tables/[number]/_components'
+
+export default function TableNumberPage() {
+  return <GuestLoginForm />
+}
